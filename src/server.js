27edcb4129cd62dc7f70331431fd5883138d796d
pakdonghun <kordn2004@gmail.com>
2022-05-03 23:06:41 +0900

    first commit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from 'ws';
 import express from "express";
 import path from 'path';
 const __dirname = path.resolve();
@@ -8,8 +10,14 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/src/views");
 app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (req,res) => res.render("home"));
+app.get("/*", (req,res) => res.redirect("/"));
  
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen); 
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+server.listen(3000, handleListen);
 
