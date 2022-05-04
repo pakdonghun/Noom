88b8a91c7276399b0b34994cdcf893c1e2fc1cb6
pakdonghun <kordn2004@gmail.com>
2022-05-04 21:47:21 +0900

    first commit

--- src/server.js
+++ src/server.js
@@ -19,11 +19,14 @@ const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server });
 
-function handleConnection(socket){
-    console.log(socket);
-}  
-
-wss.on("connection", handleConnection)
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+      });
+    socket.send("hello!!!");
+})
  
 server.listen(3000, handleListen);
 
