dc3fd9cae5afe8047a08a2a627e239ef8d1dd64c
pakdonghun <kordn2004@gmail.com>
2022-05-09 14:39:05 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -23,12 +23,20 @@ const sockets = [];
 
  wss.on("connection", (socket) => {
    sockets.push(socket);
+   socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅");
     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.on("message", (message) => {
-      const messageString = message.toString('utf8');
-      sockets.forEach(aSocket => aSocket.send(messageString));
-      });
+    socket.on("message", (msg) => {
+      const message = JSON.parse(msg);
+      switch (message.type){
+        case "new_message":
+          sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+          break
+        case "nickname":
+          socket["nickname"] = message.payload;
+          break
+      }
+      }); 
   }); 
  
 server.listen(3000, handleListen);
