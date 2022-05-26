b329274371468d90106b200b3d8a7873c1190235
pakdonghun <kordn2004@gmail.com>
2022-05-26 20:37:58 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -31,6 +31,10 @@ function publicRooms(){
   return publicRooms;
 };
 
+function countRoom(roomName){
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anon";
   socket.onAny((event) => {
@@ -40,11 +44,15 @@ wsServer.on("connection", (socket) => {
     console.log(roomName);
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome", socket.nickname); 
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+    wsServer.sockets.emit("room_change", publicRooms()); 
   });
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
+    socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)); 
   }); 
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
   socket.on("new_message", (msg, room, done) => {
     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
