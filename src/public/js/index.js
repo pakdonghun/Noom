fbd407f21d5a27e90f152bc4c00f365d29269b6d
pakdonghun <kordn2004@gmail.com>
2022-06-03 16:53:58 +0900

    commit

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -97,9 +97,10 @@ async function startMEdia(){
     makeConnection();
 }
 
-function handleWelcomeSubmit(event){
+async function handleWelcomeSubmit(event){
     event.preventDefault();
     const input = welcomeForm.querySelector("input");
+    await startMEdia();
     socket.emit("join_room", input.value, startMEdia);
     roomName = input.value;
     input.value = "";
@@ -111,16 +112,42 @@ socket.on("welcome", async () => {
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer)
     socket.emit("offer", offer, roomName);
-    console.log(offer);
+    console.log("sent the offer");
 });
 
-socket.on("offer", (offer) => {
-    console.log(offer);
+socket.on("offer", async(offer) => {
+    console.log("received the offer");
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer)
+    socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
+});
+
+socket.on("answer", (answer) => {
+    console.log("received the answer");
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", ice => {
+    myPeerConnection.addIceCandidate(ice);
 })
 
 function makeConnection(){
     myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
     myStream
     .getTracks()
     .forEach(track => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data){
+    socket.emit("ice", data.candidate, roomName); 
+}
+
+function handleAddStream(data){
+    const peersStream = document.getElementById("peersStream")
+    console.log("Peer's Stream", data.stream);
+    peersStream.srcObject = data.stream;
 }
